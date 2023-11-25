5ad9ff5db73dbcd4eae3ff21b53bb2f9c3e79c5e
Ahmad Bagheri <u.ahmadbagheri@gmail.com>
2023-11-25 22:49:41 +0330

    project initialization part2

--- client/src/assets/scripts/index.tsx
+++ client/src/assets/scripts/index.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Layout } from "./components/Layout";
+import { FirstPage } from "./pages/FirstPage";
+
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route>
-
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<FirstPage />} />
                 </Route>
             </Routes>
         </BrowserRouter>
