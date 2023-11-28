2218f878194477f273bfc23d5f72bd5ebb3d044b
Ahmad Bagheri <u.ahmadbagheri@gmail.com>
2023-11-28 16:16:27 +0330

    fix runnig project on web browser but still has some problem with connection of electron and react

--- client/src/assets/scripts/components/Layout.tsx
+++ client/src/assets/scripts/components/Layout.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 
 export function Layout() {
-    return (
-        <>
-    
-        </>
-    );
+  return (
+    <>
+      <Outlet />
+    </>
+  );
 }
--- client/src/assets/scripts/pages/FirstPage.tsx
+++ client/src/assets/scripts/pages/FirstPage.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export function FirstPage() {
     return (
         <>
-            
+            <h1>hello world in FirstPage.tsx</h1>
         </>
     );
 }
